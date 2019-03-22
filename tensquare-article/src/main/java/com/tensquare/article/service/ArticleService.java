4a0db4bb8da3d8abf45f4a1a2d8b05012d0591c9
13241917748 <13241917748@163.com>
2019-03-22 17:39:58 +0800

    吐槽服务

--- tensquare-article/src/main/java/com/tensquare/article/service/ArticleService.java
+++ tensquare-article/src/main/java/com/tensquare/article/service/ArticleService.java
@@ -1,33 +1,24 @@
 package com.tensquare.article.service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import javax.persistence.criteria.Selection;
-
-import com.mysql.cj.util.TimeUtil;
-import net.bytebuddy.asm.Advice;
+import com.tensquare.article.dao.ArticleDao;
+import com.tensquare.article.pojo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
 import org.springframework.transaction.annotation.Transactional;
 import util.IdWorker;
 
-import com.tensquare.article.dao.ArticleDao;
-import com.tensquare.article.pojo.Article;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * article服务层
